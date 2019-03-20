94292f2c6e7cd59a17d0cbab277e4e9490f26a2f
Yuki <yuki@yukis-mbp.lan>
2019-03-20 16:04:32 -0500

    First commit

---
+++ database/migrations/2019_03_17_205924_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->text('description');
+            $table->char('name', 100);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,55 @@
+@extends('tasks.layout')
+   
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Task</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('tasks.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+  
+    <form action="{{ route('tasks.update',$task->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Title:</strong>
+                    <input type="text" name="name" value="{{ $task->name }}" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Detail:</strong>
+                    <textarea class="form-control" style="height:200px" name="description" 
+                    placeholder="Description">{{ $task->description }}</textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+         <!-- Default unchecked disabled -->
+        <div class="custom-control custom-checkbox">
+        <input type="checkbox" class="custom-control-input" id="defaultUncheckedDisabled2">
+        <label class="custom-control-label" for="defaultUncheckedDisabled2">Completed</label>
+        </div>
+   
+    </form>
+@endsection
